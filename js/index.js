a4709c3f07d1e986605e3f23dcdd45579628e880
Max Bobrov <maxbobrov1997@gmail.com>
2022-07-16 19:58:09 +0300

    play with slick

--- js/index.js
+++ js/index.js
@@ -3,28 +3,13 @@ $(document).ready(function () {
     // $('button').click(function () {
     //     $('div').removeClass('flex');
     // });
-    // $('.slick-slader').slick({
-    //     arrows: false,
-    //     dots: true,
-    //     infinite: true,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 3,
-    //     responsive: [
-    //         {
-    //           breakpoint: 767,
-    //           settings: {
-    //             slidesToShow: 2,
-    //             slidesToScroll: 2
-    //           }
-    //         },
-    //         {
-    //           breakpoint: 480,
-    //           settings: {
-    //             slidesToShow: 1,
-    //             slidesToScroll: 1
-    //           }
-    //         }
-    //       ]
+    $('.slick-slader').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: false,
+        variableWidth: true
             
-    // })
+    })
 });
