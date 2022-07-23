573197f4480b3a5d20741a4b5713c797979e67a0
Max Bobrov <maxbobrov1997@gmail.com>
2022-07-23 11:40:54 +0300

    slick for testimoials

--- js/index.js
+++ js/index.js
@@ -12,4 +12,20 @@ $(document).ready(function () {
         variableWidth: true
             
     })
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        asNavFor: '.slider-for',
+        arrows: true,
+        focusOnSelect: true
+      });
+      
 });
