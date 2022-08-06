e36c0637ea455007431e4064fc64e2a5ead640db
Max Bobrov <maxbobrov1997@gmail.com>
2022-08-06 15:48:40 +0300

    some changes

--- js/index.js
+++ js/index.js
@@ -1,49 +1,46 @@
 
 $(document).ready(function () {
-    // $('button').click(function () {
-    //     $('div').removeClass('flex');
-    // });
+  // $('button').click(function () {
+  //     $('div').removeClass('flex');
+  // });
 
-    // slick-sliders
-    $('.slick-slader').slick({
-        dots: true,
-        infinite: true,
-        arrows: false,
-        speed: 300,
-        slidesToShow: 1,
-        centerMode: false,
-        variableWidth: true
-            
-    })
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav',
-        autoplay: true,
-        autoplaySpeed: 4000,
-      });
-      $('.slider-nav').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        asNavFor: '.slider-for',
-        arrows: true,
-        focusOnSelect: true
-      });
+  // slick-sliders
+  $('.slick-slader').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: false,
+    variableWidth: true
 
-      $('.products-slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 3000
-      });
-      
+  })
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav',
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    asNavFor: '.slider-for',
+    arrows: true,
+    focusOnSelect: true
+  });
 
-      $('dt').click(function(){
-        $('dd').toggleClass('hidden')
-      })
+  $('.products-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+
+
 });
